fcf5c91e0bae148cd88c45f3c316c13e39d04823
MrChio-0 <sb31989@gmail.com>
2026-08-24 23:57:01 +0800

    Refactor CONFIG object structure in config.js

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 // config.js
-window.CONFIG = window.CONFIG || {};
+window.CONFIG = {
+    // 請貼上 Google 試算表發佈的 CSV 網址
+    GOOGLE_SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2T47Bm3yVRZDm3aHMq4a2c0j_t4joMyHxRidRpOwn08K9Xmmq3X-OvkjiQFka8mm3pF0HzK3wFTOv/pub?output=csv',
 
-window.CONFIG.GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2T47Bm3yVRZDm3aHMq4a2c0j_t4joMyHxRidRpOwn08K9Xmmq3X-OvkjiQFka8mm3pF0HzK3wFTOv/pub?output=csv';
-window.CONFIG.SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxmCCRD3UyOmwybieiyEA_N11krjD1OA3UPiu-_XnH7r6bwaQpV9b7rNMVCTT9Reww/exec';
-window.CONFIG.DEFAULT_TEXT = window.CONFIG.DEFAULT_TEXT || "Type the key indicated by the blue light to begin.";
-window.CONFIG.DEFAULT_ARTICLE = window.CONFIG.DEFAULT_ARTICLE || "Type the key indicated by the blue light to begin.";
-window.CONFIG.GAME_DURATION = 300;
+    // 請貼上 Google Apps Script 部署後的 Web App 網址
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxmCCRD3UyOmwybieiyEA_N11krjD1OA3UPiu-_XnH7r6bwaQpV9b7rNMVCTT9Reww/exec',
+
+    DEFAULT_TEXT: "Type the key indicated by the blue light to begin.",
+    DEFAULT_ARTICLE: "Type the key indicated by the blue light to begin.",
+    GAME_DURATION: 300
+};

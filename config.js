69e1810ff86b8b8252bf851ebf918777aa2b010e
MrChio-0 <sb31989@gmail.com>
2026-08-25 11:35:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.CONFIG = {
     GOOGLE_SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2T47Bm3yVRZDm3aHMq4a2c0j_t4joMyHxRidRpOwn08K9Xmmq3X-OvkjiQFka8mm3pF0HzK3wFTOv/pub?output=csv',
 
     // 2. 貼上剛才複製的 Web 應用程式 URL (結尾必須是 /exec)
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx_ZeCkAkp41cIaxIkt7BhKPA-UY6qbJshd5sN74iiJyGs1tQwQlEuBt6Mznsy0Bb96/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxxcQ09Km736wQyodXmN2xOQhbX0I00HpbNj4JsTJewpSpwHjPemoAnnzLrLqFAfdEu/exec',
 
     DEFAULT_TEXT: "Type the key indicated by the blue light to begin.",
     DEFAULT_ARTICLE: "Type the key indicated by the blue light to begin.",

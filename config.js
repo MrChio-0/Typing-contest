6978cddd9b76753fd20d17fc2319d4c0bd404dc6
MrChio-0 <sb31989@gmail.com>
2026-08-25 11:08:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-// config.js
 window.CONFIG = {
-    // 請貼上 Google 試算表發佈的 CSV 網址
+    // 1. Google 試算表發佈的 CSV 網址
     GOOGLE_SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2T47Bm3yVRZDm3aHMq4a2c0j_t4joMyHxRidRpOwn08K9Xmmq3X-OvkjiQFka8mm3pF0HzK3wFTOv/pub?output=csv',
 
-    // 請貼上 Google Apps Script 部署後的 Web App 網址
+    // 2. 貼上剛才複製的 Web 應用程式 URL (結尾必須是 /exec)
     SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxmCCRD3UyOmwybieiyEA_N11krjD1OA3UPiu-_XnH7r6bwaQpV9b7rNMVCTT9Reww/exec',
 
     DEFAULT_TEXT: "Type the key indicated by the blue light to begin.",

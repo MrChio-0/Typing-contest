c46d907dcbc15c2c728177b1d349432c8e584461
MrChio-0 <sb31989@gmail.com>
2026-08-25 20:27:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.CONFIG = {
     // 1. Google 試算表發佈的 CSV 網址
-    GOOGLE_SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2T47Bm3yVRZDm3aHMq4a2c0j_t4joMyHxRidRpOwn08K9Xmmq3X-OvkjiQFka8mm3pF0HzK3wFTOv/pub?output=csv',
+    GOOGLE_SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQJtzXmCY4_XMvxs09HKHJzrUHScup6ZJC2957-UNF3Ay-vLoPANct6hUavzcYpZxH2zaehcBC_NuXz/pub?output=csv',
 
     // 2. 貼上剛才複製的 Web 應用程式 URL (結尾必須是 /exec)
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxAMV0F_75weZNh7Hhx4vN-FfBeXu6Mj6Co2iKD96Myc5PDQQZ9i71_vyvMxeW7kjZH/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxaNbx9UC0Aq31isQd6kefxQai-9LrjKXuzaCnNZ6Bxw5Q2j8Y8yk0YPXRa7gl_MOVL/exec',
 
     DEFAULT_TEXT: "Type the key indicated by the blue light to begin.",
     DEFAULT_ARTICLE: "Type the key indicated by the blue light to begin.",
